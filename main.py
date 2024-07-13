21545b9b84e25a82350e43ae208faaa90ad59ec0
Bhavya Patel <127996743+BhavyaP45@users.noreply.github.com>
2024-07-13 14:39:41 -0400

    Generate board based on start and end coordinates
    Created a function that creates a

--- main.py
+++ main.py
@@ -3,9 +3,24 @@ import time
 import os
 
 #***Define Functions***
-def opening_game():
-  opening_image = pg.image.load(os.path.join("TICTACTOE GAME", "Images", "mario.png"))
-  screen.blit(opening_image, (100, 100))
+#A function that generates a grid based on the start and end coordinates, number of squares
+def create_grid(screen, x_squares, y_squares, colour = (0, 0, 0), x = (50, 350), y = (100, 400)):
+  x_width = (x[1] - x[0])/ x_squares
+
+  for i in range(x_squares + 1):
+    x_coord = x[0] + i * x_width
+    pg.draw.line(screen, colour, (x_coord, y[0]), (x_coord, y[1]))
+
+  y_width = (y[1] -y[0])/ y_squares
+  for i in range(y_squares + 1):
+    y_coord = y[0] + i * y_width
+    pg.draw.line(screen, colour, (x[0], y_coord), (x[1], y_coord))
+
+
+def main_screen():
+  opening_image = pg.image.load(os.path.join("Images", "mario.png"))
+  create_grid(screen, 3, 3)
+
   pg.display.update()
   time.sleep(1)
 
@@ -19,7 +34,7 @@ pg.display.set_caption("Tic-Tac-Toe Game Window")
 
 running = True
 while running:
-    opening_game()
+    main_screen()
     event = pg.event.poll()
     if event.type == pg.QUIT:
         running=0
