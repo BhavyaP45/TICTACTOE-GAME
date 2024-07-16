b645f917168a798be0ad0561d49ddcf72a410f8d
Zaneeboii <zane.is.457@gmail.com>
2024-07-15 23:26:34 -0400

    worked together on liveshare

--- main.py
+++ main.py
@@ -1,3 +1,4 @@
+# Import libraries
 import pygame as pg
 import pygame_menu
 import pygame_gui
@@ -13,16 +14,18 @@ pg.init()
 
 # Screen dimensions
 SCREEN_WIDTH = 400
-SCREEN_HEIGHT = 500
+SCREEN_HEIGHT = 600
 
 # counter 
 trigger_counter = 0
 
 # set up usernames
-player1_username = ''
-player2_username = ''
+player1_username = 'Player 1'
+player2_username = 'Player 2'
 winner_username = ''
 loser_username = ''
+raw_player1_colour = 'red'
+raw_player2_colour = 'red'
 
 # utility function to read json files
 def read_json_file(file_path):
@@ -56,7 +59,7 @@ def update_gamelog():
     # New entry to append
     new_entry = {
         "timestamp": datetime.now(timezone.utc).strftime('%Y-%m-%dT%H:%M:%SZ'),
-        "winner": winner_username 
+        "winner": player1_username if winner == 1 else player2_username, 
     }
 
     # Path to the JSON file
@@ -65,11 +68,10 @@ def update_gamelog():
     # Append the new entry
     append_entry(file_path, new_entry)
 
-
-
 # Define colours and line thickness
-green =(0, 255, 0)
+green =(0, 0, 0)
 red = (255, 0, 0)
+white = (255, 255, 255)
 blue = (0, 0, 255)
 orange =(252, 152, 3)
 yellow = (252, 227, 3)
@@ -92,8 +94,7 @@ winner = 0
 score1_counter = 0
 score2_counter = 0
 
-# Create play again button
-again_rect = Rect(SCREEN_WIDTH // 2 - 80, SCREEN_HEIGHT // 2, 160, 50)
+
 
 # Define font
 font = pg.font.SysFont(None, 40)
@@ -103,7 +104,7 @@ font = pg.font.SysFont(None, 40)
 #A function that generates a grid based on the start and end coordinates, number of squares
 # Inputs: Screen, number of squares in the x and y directions, colour, start and end coordinates in the x and y directions
 # Outputs: list of x_coordinates, list of y_coordinates, width and height of boxes
-def create_grid(screen, x_squares, y_squares, colour = (0, 0, 0), x = (50, 350), y = (100, 400)):
+def create_grid(screen, x_squares, y_squares, colour = (255, 255, 255), x = (50, 350), y = (100, 400)):
   #Calculate width of each box
   x_width = (x[1] - x[0])/ x_squares
   #Initialize x list
@@ -127,7 +128,7 @@ def create_grid(screen, x_squares, y_squares, colour = (0, 0, 0), x = (50, 350),
 #A function that reads the markers list and draws corresponding markers (X or O)
 #Inputs: Side lengths of each box
 def draw_markers(box_side_length):
-  global markers, x_list, y_list, line_width
+  global markers, x_list, y_list, line_width, player1_colour, player2_colour
 
   # Display Win counter for each player
   score_counter()
@@ -144,17 +145,17 @@ def draw_markers(box_side_length):
           #1 is used in the marker list to indicate an X
           if y == 1:
               #Create start and end coordinates for both diagonal lines in an x
-              start_coord1 = (x_pos * box_side_length + 50 + scale, y_pos * box_side_length + 100 + scale)
-              end_coord1 = (x_pos * box_side_length + box_side_length + 50 - scale , y_pos * box_side_length + box_side_length + 100 - scale)
-              start_coord2 = (x_pos * box_side_length + 50 + scale, y_pos * box_side_length + box_side_length + 100 - scale)
-              end_coord2 = (x_pos * box_side_length + box_side_length + 50 - scale , y_pos * box_side_length + 100 + scale)
+              start_coord1 = (x_pos * box_side_length + 50 + scale, y_pos * box_side_length + 150+ scale)
+              end_coord1 = (x_pos * box_side_length + box_side_length + 50 - scale , y_pos * box_side_length + box_side_length + 150- scale)
+              start_coord2 = (x_pos * box_side_length + 50 + scale, y_pos * box_side_length + box_side_length + 150- scale)
+              end_coord2 = (x_pos * box_side_length + box_side_length + 50 - scale , y_pos * box_side_length + 150+ scale)
               #Draw the lines on the screen
               pg.draw.line(screen, player1_colour, start_coord1, end_coord1, line_width)
               pg.draw.line(screen, player1_colour, start_coord2, end_coord2, line_width)
           #-1 is used in the marker list to indicate an O
           if y == -1:
               #Draw circle on board
-              pg.draw.circle(screen, player2_colour, (x_pos * box_side_length + box_side_length//2 + 50, y_pos * box_side_length + box_side_length//2 + 100), box_side_length//2 - scale, line_width)
+              pg.draw.circle(screen, player2_colour, (x_pos * box_side_length + box_side_length//2 + 50, y_pos * box_side_length + box_side_length//2 + 150), box_side_length//2 - scale, line_width)
       #Increment both counters  
           y_pos += 1
       x_pos += 1
@@ -168,46 +169,57 @@ greenav = pg.image.load('Images/GreenAvatar.png')
 blueav = pg.image.load('Images/BlueAvatar.png')
 purpleav = pg.image.load('Images/PurpleAvatar.png')
 pinkav = pg.image.load('Images/PinkAvatar.png')
+
 avatar_list = [redav, orangeav, yellowav, greenav, blueav, purpleav, pinkav]
 
-player1_colour = (255, 0, 0)   # Example: Red color
-player2_colour = (0, 255, 0)   # Example: Green color
-avatar1 = avatar_list[0]       # Initial avatar for player 1 (red)
-avatar2 = avatar_list[1]
+for avatar in avatar_list:
+  avatar = pg.transform.scale(avatar, (200, 200))
+
+avatar1 = avatar_list[0]       
+avatar2 = avatar_list[0]
 
 # Set colours and avatars based on menu input
 def set_colour1(value, colour):
-    global player1_colour, avatar1
+    global player1_colour, avatar1, raw_player1_colour
     player1_colour = colour_list[colour-1]
     avatar1 = avatar_list[colour-1]
+    raw_player1_colour = colour_list[colour-1]
 
 def set_colour2(value, colour):
-    global player2_colour, avatar2
+    global player2_colour, avatar2, raw_player2_colour
     player2_colour = colour_list[colour-1]
     avatar2 = avatar_list[colour-1]
+    raw_player2_colour = colour_list[colour-1]
 
 #A function that loops through the main screen in the program 
 def main_screen():
   #Access all the global variables
   global x_list, y_list, clicked, position, markers, player, running, click_counter, game_over, winner, event, again_rect
-  global score1_counter, score2_counter
+  global score1_counter, score2_counter, avatar1, avatar2
 
   #Initialize boolean that will continue running until the program is quit
   running = True
   while running:
     #Background for the screen
-    bg = (150, 255, 255)
-    screen.fill(bg)
+    bg = pg.image.load("Images/bg.png")
+    screen.blit(bg, (0, 0))
 
     #Initialize start and end coordinates for the grid
     x = (50, 350)
-    y = (100, 400)
+    y = (150, 450)
 
     x_list, y_list, x_width, y_width = create_grid(screen, 3, 3, x = x, y = y)
 
-    screen.blit(avatar1, (10, 10))
-    screen.blit(avatar2, (300, 10))
+    # image = pygame.image.load('your_image.png')
+    new_size = (75, 75)
+    avatar1 = pg.transform.scale(avatar1, new_size)
+    avatar2 = pg.transform.scale(avatar2, new_size)
+    # screen.blit(scaled_image, (50, 50))
 
+    screen.blit(avatar1, (90, 60))
+    screen.blit(avatar2, (230, 60))
+
+    
     #Draw the markers based on the list 
     draw_markers(x_width)
 
@@ -256,28 +268,15 @@ def main_screen():
     
     #If the game is finished, display the winner
     if game_over == True:
-          display_winner(winner)
-          
-          # update_gamelog()
           global trigger_counter
           if trigger_counter == 0:
             update_gamelog()
+            fetchGameLog()
             trigger_counter += 1
+
+          display_winner(winner)
           
-          # check to see if user plays again
-          if event.type == pg.MOUSEBUTTONDOWN and clicked == False:
-              clicked = True
-          if event.type == pg.MOUSEBUTTONUP and clicked == True:
-              clicked = False
-              position = pg.mouse.get_pos()
-              if again_rect.collidepoint(position):
-                  # rest variables
-                  position = []
-                  player = 1
-                  winner = 0
-                  game_over = False
-                  click_counter = 0
-                  markers = [[0, 0, 0], [0, 0, 0], [0, 0, 0]]
+
 
     #Update the display
     pg.display.update()
@@ -286,7 +285,8 @@ def main_screen():
 #Inputs: x and y coordinates
 #Outputs: Row and column numbers (0 indexing)
 def gameReset():
-  global x_list, y_list, clicked, position, markers, player, running, click_counter, game_over, winner, event, again_rect
+  global x_list, y_list, clicked, position, markers, player, running, click_counter, game_over, winner, event, trigger_counter
+  trigger_counter = 0
   position = []
   player = 1
   winner = 0
@@ -310,6 +310,14 @@ def get_row_column(x, y):
       row = i
 
   return col, row
+
+def fetchGameLog():
+  with open('gamelog.json', 'r') as file:
+    game_log_data = json.load(file)
+    for entry in game_log_data:
+      timestampData = entry['timestamp']
+      winner = entry['winner']
+      game_log.add.label(f"Winner: {winner} - {timestampData}", font_size=10)
 
 # Determine the winner (player 1 or player 2, or tie game)
 def check_winner():
@@ -355,92 +363,69 @@ def check_winner():
         
 # Displays the outcome of the game, and a button to play again
 def display_winner(winner):
-  global end_menu
-  # Display which player won, otherwise display "tie game"
-  if winner > 0:
-      win_text = "Player " + str(winner) + " wins!"
-      win_img = font.render(win_text, True, blue)
-      pg.draw.rect(screen, green, (SCREEN_WIDTH // 2 - 100, SCREEN_HEIGHT // 2 - 110, 200, 50))
-      screen.blit(win_img, (SCREEN_WIDTH // 2 - 100, SCREEN_HEIGHT // 2 - 100))
-  else:
-      win_text = "Tie Game!"
-      win_img = font.render(win_text, True, blue)
-      pg.draw.rect(screen, green, (SCREEN_WIDTH // 2 - 70, SCREEN_HEIGHT // 2 - 110, 140, 50))
-      screen.blit(win_img, (SCREEN_WIDTH // 2 - 70, SCREEN_HEIGHT // 2 - 100))
+  global end_menu, SCREEN_HEIGHT
   
-  if winner > 0:
-      winner_username = get_winner_username()
+  winner_username = get_winner_username()
 
-      if winner == 1:
-      
-          winner_username = player1_username
-      else:
-          
-          winner_username = player2_username
-      print("winner is - " + winner_username )
-        
+  if winner == 1:
+      winner_username = player1_username
+      win_text = "" + str(player1_username) + " wins!"
+
+  elif winner == 2:
+      winner_username = player2_username
       win_text = "" + str(winner_username) + " wins!"
       
-      # update_leaderboard()
-      win_img = font.render(win_text, True, blue)
-      
-      # pygame_menu.events.RESET
-
-      # menu._open(end_menu)
-      gameReset()
-      end_menu = pygame_menu.Menu('GAME OVER', 400, 500,
-                    theme=space_theme)
-      end_menu.add.label(f'{str(winner_username)} wins!', font_size=24)
-      end_menu.add.button('Change Avatars', avatar_endmenu)
-      end_menu.add.button('Play Again!', main_screen)
-      end_menu.add.button('Quit', pygame_menu.events.EXIT)
-      end_menu.mainloop(screen)
-
-
-      pg.draw.rect(screen, green, (SCREEN_WIDTH // 2 - 100, SCREEN_HEIGHT // 2 - 110, 200, 50))
-      screen.blit(win_img, (SCREEN_WIDTH // 2 - 100, SCREEN_HEIGHT // 2 - 100))
-      
-      
   else:
       win_text = "Tie Game!"
-      win_img = font.render(win_text, True, blue)
-      pg.draw.rect(screen, green, (SCREEN_WIDTH // 2 - 70, SCREEN_HEIGHT // 2 - 110, 140, 50))
-      screen.blit(win_img, (SCREEN_WIDTH // 2 - 70, SCREEN_HEIGHT // 2 - 100))
-
-  # Play again button
-  again = "Play Again?"
-  again_img = font.render(again, True, blue)
-  pg.draw.rect(screen, green, again_rect)
-  screen.blit(again_img, (SCREEN_WIDTH // 2 - 80, SCREEN_HEIGHT // 2 + 10))
+  
     
+  # update_leaderboard()
+  win_img = font.render(win_text, True, blue)
+  
+  # pygame_menu.events.RESET
+
+  # menu._open(end_menu)
+  gameReset()
+  end_menu = pygame_menu.Menu('GAME OVER', 400, SCREEN_HEIGHT,
+                theme=space_theme)
+  end_menu.add.label(win_text, font_size=24)
+  end_menu.add.button('Change Avatars', avatar_endmenu)
+  end_menu.add.button('Game Log', game_log_endmenu)
+  end_menu.add.button('Play Again!', main_screen)
+  end_menu.add.button('Quit', pygame_menu.events.EXIT)
+  end_menu.mainloop(screen)
 
 # Displays # of wins for each player
 def score_counter():
-  
+  p1_img = fontM.render(player1_username, True, white)
+  p2_img = fontM.render(player2_username, True, white)
+  screen.blit(p1_img, (30, 40))
+  screen.blit(p2_img, (230, 40))
+
   score1 = "Wins: " + str(score1_counter)
-  score1_img = font.render(score1, True, blue)
-  pg.draw.rect(screen, green, (40, 50, 100, 23))
-  screen.blit(score1_img, (40, 50))
+  score1_img = font.render(score1, True, white)
+
+  screen.blit(score1_img, (40, 90))
 
   score2 = "Wins: " + str(score2_counter)
-  score2_img = font.render(score2, True, blue)
-  pg.draw.rect(screen, green, (270, 50, 100, 23))
-  screen.blit(score2_img, (270, 50))
+  score2_img = font.render(score2, True, white)
 
+  screen.blit(score2_img, (300, 90))
 
+  currentTurn = (str(player1_username) + "'s turn " if player == 1 else str(player2_username) + "'s turn ")
+  currentTurn_img = fontXL.render(currentTurn, True, white)
 
-  again = "Play Again?"
-  again_img = font.render(again, True, blue)
-  pg.draw.rect(screen, green, again_rect)
-  screen.blit(again_img, (SCREEN_WIDTH // 2 - 80, SCREEN_HEIGHT // 2 + 10))
+  screen.blit(currentTurn_img, (65, 500))
 
 
 
 pg.font.init()
 font = pg.font.Font(None, 24)
+fontM = pg.font.Font(None, 40)
+fontXL = pg.font.Font(None, 50)
 
-screen = pg.display.set_mode((400,500))
-manager = pygame_gui.UIManager((400,500))
+screen = pg.display.set_mode((400, SCREEN_HEIGHT))
+manager = pygame_gui.UIManager((400, SCREEN_HEIGHT))
 pg.display.set_caption("Space Tac Toe")
 x_list = []
 y_list = []
@@ -453,43 +438,53 @@ space_theme = pygame_menu.themes.THEME_DARK.copy()
 # Set usernames based on menu input
 def set_name1(name):
   global player1_username
-  player1_username = name
+  player1_username = name[:10]
 
 def set_name2(name):
   global player2_username
-  player2_username = name
+  player2_username = name[:10]
 
 # Opening the "Avatar Selection" tab from the menu
 def avatar_menu():
   menu._open(avatars)
 
-# Creating the main menu
+# Creating the game_log_menu
+def game_log_endmenu():
+  global end_menu
+  end_menu._open(game_log)
+
 def avatar_endmenu():
   global end_menu
   end_menu._open(avatars)
 
+game_log = pygame_menu.Menu('GAME LOG', 400, SCREEN_HEIGHT,
+            theme=space_theme)
+game_log.add.label("Past Games:", font_size=24)
+# Create a scrollable area
+if game_over == True:
+   fetchGameLog()
+
+# Add some dummy data to the scrollable area
+# for i in range(1, 21):
+#     scroll_area.add.label(f"Game {i}: Player X won", font_size=20)
 
 
-menu = pygame_menu.Menu('Space Tac Toe', 400, 500,
+
+menu = pygame_menu.Menu('Space Tac Toe', 400, SCREEN_HEIGHT,
                        theme=space_theme)
 
 # Menu buttons
+
 menu.add.button('Avatars', avatar_menu)
 menu.add.button('Play', main_screen)
 menu.add.button('Quit', pygame_menu.events.EXIT)
-
+menu.add.image("Images/earth.png", scale = (0.5, 0.5))
 # Creating the "Avatar Selection" menu and its inputs
-avatars = pygame_menu.Menu('Select an Avatar', 400, 500,
-                       theme=pygame_menu.themes.THEME_BLUE)
-avatars.add.text_input('Player 1: ', default='Name', onchange=set_name1)
+avatars = pygame_menu.Menu('Select an Avatar', 400, SCREEN_HEIGHT, theme=pygame_menu.themes.THEME_DARK)
+avatars.add.text_input('Player 1: ', default='John Doe', onchange=set_name1)
 avatars.add.selector('Colour: ', [('Red', 1), ('Orange', 2), ('Yellow', 3), ('Green', 4), ('Blue', 5), ('Purple', 6), ('Pink', 7)], onchange=set_colour1)
-avatars.add.text_input('Player 2: ', default='Jane Smith')
-
-avatars.add.text_input('Player 2: ', default='Name')
+avatars.add.text_input('Player 2: ', default='Jane Smith', onchange=set_name2)
 avatars.add.selector('Colour: ', [('Red', 1), ('Orange', 2), ('Yellow', 3), ('Green', 4), ('Blue', 5), ('Purple', 6), ('Pink', 7)], onchange=set_colour2)
-
-
-
 
 running = True
 while running:
